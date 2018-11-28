4596e64c580e5563e4a9138af5551a3415996da8
Ahmed Marzook <m4marzook@gmail.com>
2018-11-28 14:45:53 +0000

    chapter 3 javabuzz and control flow done

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -1,5 +1,5 @@
 // what imports will you need?
-
+import java.util.Scanner;
 // can you remember how to set up your main function?
 
 // find a way to read in user input
@@ -8,3 +8,16 @@
 // (assume user only ever enters integers)
 
 // print the answer to the console
+class controlFlow {
+    public static void main(String[] args){
+        System.out.println("Enter a Integer: ");
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+
+        if(userInput%2 != 0){
+            System.out.println(userInput +" is a odd number");
+        }else{
+            System.out.println(userInput +" is a even number");
+        }
+    }
+}
