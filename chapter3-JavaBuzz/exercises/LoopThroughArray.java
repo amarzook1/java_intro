0c9232f6c595ec6ea65e6189d086c8cdc4b2103e
Ahmed Marzook <m4marzook@gmail.com>
2018-11-28 15:04:35 +0000

    Chapter 3 complete

--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,12 @@
 // can you remember how to set up your main function?
 
 // loop through an integer array and print out each element
+public class loopArray {
+    public static void main(String[] args){
+        Integer[] integerArray = {12,13,124,45};
+
+        for(int tempInt : integerArray){
+            System.out.println(tempInt);
+        }
+    }
+}
