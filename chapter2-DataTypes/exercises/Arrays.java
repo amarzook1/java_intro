9cc751bcaa00bab209455f69fef4fdf10ad3471c
Ahmed Marzook <m4marzook@gmail.com>
2018-11-28 11:45:29 +0000

    chapter 2 array stuff

--- chapter2-DataTypes/exercises/Arrays.java
+++ chapter2-DataTypes/exercises/Arrays.java
@@ -5,3 +5,30 @@
 // declare an array of integers and print it
 
 // declare an ArrayList of integers, add numbers to it, and then print it
+import java.util.ArrayList;
+class ArrayStuff {
+  public static void main(String[] args) {
+    String[] stringArray = {"jhon","sam","bob"};
+
+    for(String temp : stringArray){
+      System.out.println(temp);
+    }
+
+    Integer[] integerArray = {1,2,3,4};
+
+    for(Integer temp : integerArray){
+      System.out.println(temp);
+    }
+
+    ArrayList<Integer> arrayListNumbers = new ArrayList<Integer>();
+    arrayListNumbers.add(2);
+    arrayListNumbers.add(4);
+    arrayListNumbers.add(1);
+    arrayListNumbers.add(14);
+
+    for(Integer temp : arrayListNumbers){
+      System.out.println(temp);
+    }
+
+  }
+}
